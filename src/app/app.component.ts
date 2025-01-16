3d15d4ebd503ead4008dad111c95b4db52ceb884
marcelo <marcelovserrano@outlook.com>
2025-01-15 23:13:05 -0300

    criando template da aplicacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TemplateModule } from './template/template.module';
+import { NavbarComponent } from './template/navbar/navbar.component';
+import { SidebarComponent } from './template/sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, TemplateModule, NavbarComponent, SidebarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'clientes-app';
+
+  ngAfterViewInit() {
+    var path = location.pathname;
+
+    $('#layoutSidenav_nav .sb-sidenav a.nav-link').each((index: any, element: any) => {
+      if ($(element).attr('href') === path) {
+        $(element).addClass("active");
+      }
+    });
+
+    $("#sidebarToggle").on("click", function (event: { preventDefault: () => void; }) {
+      $("body").toggleClass("sb-sidenav-toggled");
+    });
+  }
+
+
+
 }
